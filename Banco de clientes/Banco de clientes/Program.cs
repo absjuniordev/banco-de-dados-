21c4a233eefdb3639b6d47e875b203acb82b24f6
absjuniordev <abs.junnior@hotmail.com>
2022-02-01 22:18:32 -0300

    Alteração no escopo
    Enxugamento da logica. Remoção de redundancias

--- Banco de clientes/Banco de clientes/Program.cs
+++ Banco de clientes/Banco de clientes/Program.cs
@@ -10,30 +10,30 @@ namespace Banco_de_Dados
     {       
         static void Main(string[] args)
         {
-            string opcaoUsuario; 
-            do
+            string opcaoUsuario;
+           do
             {
                 opcaoUsuario = ObterOpcaoUsuario();
-                switch (opcaoUsuario)
+
+                switch (opcaoUsuario.ToUpper())
                 {
                     case "1":
-                        escolhaComLimite(1);
+                        cadastroUsuario(1);
                         break;
                     case "2":
-                        escolhaSemLimite(1);
+                        Console.WriteLine("Em implementação");
                         break;
                     case "3":
-
-                        break;
-                    case "X":
                         Console.WriteLine("Saindo...");
                         break;
                     default:
-                        throw new ArgumentOutOfRangeException();
+                        Console.WriteLine("Opcão invalida");
+                        Console.ReadLine();
+                        break;
+                        
                 }
-                Console.ReadLine();
                 Console.Clear();
-            } while (opcaoUsuario.ToUpper() != "X");
+            } while (opcaoUsuario.ToUpper() != "3");
         }
 
         private static string ObterOpcaoUsuario()
@@ -42,66 +42,18 @@ namespace Banco_de_Dados
             Console.WriteLine("|============-CADASTRO DE USUARIOS-=============|");
             Console.WriteLine("|----------------Seja bem vindo!----------------|");
             Console.WriteLine("|===============================================|");
-            Console.Write("O que deseja fazer? \n1 - Cadastrar clientes de forma limitada \n2 - Cadastrar clientes de forma ilimitada \n3 - Listar usuario \nX - Sair \n: ");
+            Console.Write("O que deseja fazer? \n1 - Cadastrar clientes \n2 - Listar usuario \n3 - Sair \n: ");
             opcaoUsuario = (Console.ReadLine());
             Console.WriteLine("|-----------------------------------------------|");
             return opcaoUsuario;
         }
 
-        private static int escolhaComLimite(int limiteDesejado)
+        
+        private static int cadastroUsuario(int contt)
         {
-            Console.Write("Quantos usuarios deseja cadastrar? :");
-            limiteDesejado = int.Parse(Console.ReadLine());
-            //ira limitar a quantidade de valores a serem inseridos no vetor
-            Console.WriteLine("===================================================");
-            string[,] item = new string[limiteDesejado, 3];
-            //classe ||variavel de referencia|| alocação de espaço na memoria
-
-            string nome, telefone, endereço, respostaUsurario = "";
-            int cont2 = 1;
-
-            for (int cont = 0; cont < limiteDesejado; cont++)
-            {
-                Console.Write("Digite um nome: ");
-                nome = Console.ReadLine();
-                item[cont, 0] = nome;
-                Console.Write("Digite o endereço: ");
-                endereço = Console.ReadLine();
-                item[cont, 1] = endereço;
-                Console.Write($"Digite o telefone: ");
-                telefone = Console.ReadLine();
-                item[cont, 2] = telefone;
-                cont2++;
-                Console.Clear();
-                Console.WriteLine("==============================================");
-            }
-            Console.WriteLine("|Deseja verificar os clientes cadastados? [s/n]|");
-            respostaUsurario = Console.ReadLine();
-            switch (respostaUsurario)
-            {
-                case "s":
-                    for (int cont1 = 0; cont1 < limiteDesejado; cont1++)
-                    {
-                        Console.WriteLine("-----------------------------------------------------");
-                        Console.WriteLine($"Cadastro Nº {cont2}\nNome:{item[cont1, 0]}\nEnd:{item[cont1, 1]}\nTel:{item[cont1, 2]} ");
-                        Console.WriteLine("-----------------------------------------------------");
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Obrigado");
-                    break;
-            }
-            Console.WriteLine("Digite o numeor do cadastro");
-            Console.Write("\nDigite o indice que deseja verificar: ");
-            int num1 = int.Parse(Console.ReadLine());
-            // Console.WriteLine($"O indice corresponde ao nome: {[item[num1]}");
-            Console.ReadLine();
-            return limiteDesejado;
-        }
-
-        private static int escolhaSemLimite(int contt)
-        {
-            List<string> nome = new List<string>();           
+            List<string> nome = new List<string>();
+            List<string> enderero = new List<string>();
+            List<string> telefone = new List<string>();
             string ind, resp1 = "", resp = "";
             int cont1 = 1, conta = 0;
             do
@@ -109,7 +61,14 @@ namespace Banco_de_Dados
                 Console.Write($"Digite o {cont1}ª nome: ");
                 ind = Console.ReadLine();
                 nome.Add(ind);
-                Console.Write("Deseja continuar? [s/n]: ");
+                Console.Write($"Digite o {cont1}ª endereço: ");
+                ind = Console.ReadLine();
+                enderero.Add(ind);
+                Console.Write($"Digite o {cont1}ª telefone: ");
+                ind = Console.ReadLine();
+                telefone.Add(ind);
+
+                Console.Write("Deseja continuar cadastrando? [s/n]: ");
                 resp = Console.ReadLine();
                 conta++;
                 cont1++;
@@ -117,22 +76,26 @@ namespace Banco_de_Dados
                 Console.WriteLine("---------------------------------------------");
             } while (resp == "s");
           
-            Console.WriteLine($"Foram cadastrados {cont1} nomes.");
+            Console.WriteLine($"{cont1} Usuarios(s) cadastrado(s).");
             Console.WriteLine("---------------------------------------------");
             Console.Write("Deseja listar o(s) nome(s)? [s/n]");
             resp1 = Console.ReadLine();
             Console.WriteLine("---------------------------------------------");
-            if (resp1.ToUpper() == "s")
+            
+            if (resp1 == "s")
             {
-                string[] arr_sample = nome.ToArray();
+                string[] arr_sample1 = nome.ToArray();
+                string[] arr_sample2 = enderero.ToArray();
+                string[] arr_sample3 = telefone.ToArray();
                 for (int index = 0; index < conta; index++)
                 {
-                    System.Console.WriteLine(arr_sample[index]);
+                    System.Console.WriteLine($"Nome:{arr_sample1[index]}\nEndereço:{arr_sample2[index]}\nTelefone:{arr_sample3[index]}");
                     Console.WriteLine("---------------------------------------------");
                 }
+                Console.ReadLine();
             }
             else Console.WriteLine("Obrigado");
-            Console.ReadLine();
+            Console.WriteLine("Pressione ENTER para retornar ao MENU.");
             return contt;
         }
 
