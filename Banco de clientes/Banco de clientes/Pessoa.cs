e30233b53b6e3dc16f8de5ad220742a40a7b0b12
absjuniordev <abs.junnior@hotmail.com>
2022-02-10 22:50:42 -0300

    .

---
+++ Banco de clientes/Banco de clientes/Pessoa.cs
@@ -0,0 +1,7 @@
+﻿public class Pessoa
+{
+    public int Idade { get; set; }
+    public string Nome { get; set; }
+    public string Endereço { get; set; }
+}
+
--- Banco de clientes/Banco de clientes/Program.cs
+++ Banco de clientes/Banco de clientes/Program.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Banco_de_Dados
 {
     internal class Program
-    {       
+    {
         static void Main(string[] args)
         {
             string opcaoUsuario;
@@ -21,33 +22,33 @@ namespace Banco_de_Dados
                         cadastroUsuario(1);
                         break;
                     case "2":
-                        Console.WriteLine("Em implementação");
+                        WriteLine("Em implementação");
                         break;
                     case "3":
-                        Console.WriteLine("Saindo...");
+                        WriteLine("Saindo...");
                         break;
                     default:
-                        Console.WriteLine("Opcão invalida");
-                        Console.ReadLine();
+                        WriteLine("Opcão invalida");
+                        ReadLine();
                         break;
                         
                 }
-                Console.Clear();
+                Clear();
             } while (opcaoUsuario.ToUpper() != "3");
         }
 
         private static string ObterOpcaoUsuario()
         {
             string opcaoUsuario;
-            Console.WriteLine("|============-CADASTRO DE USUARIOS-=============|");
-            Console.WriteLine("|----------------Seja bem vindo!----------------|");
-            Console.WriteLine("|===============================================|");
-            Console.Write("O que deseja fazer? \n1 - Cadastrar clientes \n2 - Listar usuario \n3 - Sair \n: ");
-            opcaoUsuario = (Console.ReadLine());
-            Console.WriteLine("|-----------------------------------------------|");
+            WriteLine("|============-CADASTRO DE USUARIOS-=============|");
+            WriteLine("|----------------Seja bem vindo!----------------|");
+            WriteLine("|===============================================|");
+            Write("O que deseja fazer? \n1 - Cadastrar clientes \n2 - Listar usuario \n3 - Sair \n: ");
+            opcaoUsuario = (ReadLine());
+            WriteLine("|-----------------------------------------------|");
             return opcaoUsuario;
         }
-
+          
         
         private static int cadastroUsuario(int contt)
         {
@@ -55,51 +56,49 @@ namespace Banco_de_Dados
             List<string> enderero = new List<string>();
             List<string> telefone = new List<string>();
             string ind, resp1 = "", resp = "";
-            int cont1 = 1, conta = 0;
+            int ContadorDeUsuario = 1, TotalUsuario = 0;
             do
             {
-                Console.Write($"Digite o {cont1}ª nome: ");
-                ind = Console.ReadLine();
+                Write($"Digite o {ContadorDeUsuario}ª nome: "); 
+                ind = ReadLine();
                 nome.Add(ind);
-                Console.Write($"Digite o {cont1}ª endereço: ");
-                ind = Console.ReadLine();
+                Write($"Digite o {ContadorDeUsuario}ª endereço: ");
+                ind = ReadLine();
                 enderero.Add(ind);
-                Console.Write($"Digite o {cont1}ª telefone: ");
-                ind = Console.ReadLine();
+                Write($"Digite o {ContadorDeUsuario}ª telefone: ");
+                ind = ReadLine();
                 telefone.Add(ind);
 
-                Console.Write("Deseja continuar cadastrando? [s/n]: ");
-                resp = Console.ReadLine();
-                conta++;
-                cont1++;
-                Console.Clear();
-                Console.WriteLine("---------------------------------------------");
+                Write("Deseja continuar cadastrando? [s/n]: ");
+                resp = ReadLine();
+                TotalUsuario++;
+                ContadorDeUsuario++;
+                Clear();
+                WriteLine("---------------------------------------------");
             } while (resp == "s");
           
-            Console.WriteLine($"{cont1} Usuarios(s) cadastrado(s).");
-            Console.WriteLine("---------------------------------------------");
-            Console.Write("Deseja listar o(s) nome(s)? [s/n]");
-            resp1 = Console.ReadLine();
-            Console.WriteLine("---------------------------------------------");
+            WriteLine($"{TotalUsuario} Usuarios(s) cadastrado(s).");
+            WriteLine("---------------------------------------------");
+            Write("Deseja listar o(s) nome(s)? [s/n]");
+            resp1 = ReadLine();
+            WriteLine("---------------------------------------------");
             
             if (resp1 == "s")
             {
                 string[] arr_sample1 = nome.ToArray();
                 string[] arr_sample2 = enderero.ToArray();
                 string[] arr_sample3 = telefone.ToArray();
-                for (int index = 0; index < conta; index++)
+                for (int index = 0; index < TotalUsuario; index++)
                 {
                     System.Console.WriteLine($"Nome:{arr_sample1[index]}\nEndereço:{arr_sample2[index]}\nTelefone:{arr_sample3[index]}");
-                    Console.WriteLine("---------------------------------------------");
+                    WriteLine("---------------------------------------------");
                 }
-                Console.ReadLine();
+                ReadLine();
             }
-            else Console.WriteLine("Obrigado");
-            Console.WriteLine("Pressione ENTER para retornar ao MENU.");
+            else WriteLine("Obrigado");
+            WriteLine("Pressione ENTER para retornar ao MENU.");
             return contt;
-        }
-
-       
+        }       
     }
 
 }
